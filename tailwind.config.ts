ee1c4bff6cec5733af16dae220cc3a10ae0cafb5
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2024-12-17 21:19:17 +0100

    feat(assets): Added assets for zentry project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                zentry: ["zentry", "sans-serif"],
+                general: ["general", "sans-serif"],
+                "circular-web": ["circular-web", "sans-serif"],
+                "robert-medium": ["robert-medium", "sans-serif"],
+                "robert-regular": ["robert-regular", "sans-serif"],
+            },
+            colors: {
+                blue: {
+                    50: "#DFDFF0",
+                    75: "#dfdff2",
+                    100: "#F0F2FA",
+                    200: "#010101",
+                    300: "#4FB7DD",
+                },
+                violet: {
+                    300: "#5724ff",
+                },
+                yellow: {
+                    100: "#8e983f",
+                    300: "#edff66",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
